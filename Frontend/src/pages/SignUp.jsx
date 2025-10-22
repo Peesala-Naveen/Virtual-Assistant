2deb3282a610cfe950bc56b06a84dd61654a1f17
Peesala Naveen <naveenpeesala2004@gmail.com>
2025-10-22 12:27:34 +0530

    Update SignUp.jsx

--- Frontend/src/pages/SignUp.jsx
+++ Frontend/src/pages/SignUp.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useState } from 'react'
 import '../styles/sign.css'
 import { FiEye, FiEyeOff } from 'react-icons/fi'
 import { useNavigate } from 'react-router-dom'
-import { userDataContext } from '../context/userContext.jsx'
+import { userDataContext } from '../context/UserContext.jsx'
 import axios from 'axios'
 
 
