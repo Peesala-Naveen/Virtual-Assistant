65ec5d024ce89a63876b8274ea01ad919ac4d7f7
Peesala Naveen <naveenpeesala2004@gmail.com>
2026-03-10 00:20:37 +0530

    Fix context import in Home.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
 import UserContext from "./context/UserContext.jsx";
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <UserContext>
-      <BrowserRouter>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <UserContext>
         <App />
-      </BrowserRouter>
-    </UserContext>
-  </React.StrictMode>
-)
+      </UserContext>
+    </BrowserRouter>
+  </StrictMode>
+);
